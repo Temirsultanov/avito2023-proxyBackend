9cf1df2b159357c1e0d51907af627b408daff827
Мухаммад <human.twm@gmail.com>
2023-11-18 11:32:17 +0300

    feat: add delay 2s

--- server.mjs
+++ server.mjs
@@ -41,7 +41,7 @@ const server = http.createServer((request, response) => {
     return;
   }
 
-  routes[route](params).then((data) => {
+  sleep(2000).then(routes[route](params)).then((data) => {
     response.statusCode = 200;
     response.end(data);  
   }).catch((error) => {
